1802747fb586e0c996414a0dbbd90e889ef45a12
Nicolas Castillo <castillonicolas606@gmail.com>
2020-11-02 13:53:24 -0500

    First-commit

---
+++ behavior.js
@@ -0,0 +1,17 @@
+document.addEventListener("DOMContentLoaded", function(event) {
+
+  var thumbnailElement = document.getElementById ("smart_thumbnail");
+
+  thumbnailElement.addEventListener("click", function() {
+  alert("I saw you click!");
+  thumbnailElement.className = "";
+    });
+      
+  (function(d, s, id) {
+var js, fjs = d.getElementsByTagName(s)[0];
+if (d.getElementById(id)) return;
+js = d.createElement(s); js.id = id;
+js.src = "https://connect.facebook.net/en_US/sdk.js#xfbml=1&version=v3.0";
+fjs.parentNode.insertBefore(js, fjs);
+}(document, 'script', 'facebook-jssdk'));
+});
